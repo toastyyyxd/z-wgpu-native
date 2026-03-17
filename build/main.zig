490c3531a7558c8f2264daa95f53cbe526264e44
toastyyyxd <contact@toastyx.dev>
2026-03-18 07:09:15 +0800

    sync

--- build/main.zig
+++ build/main.zig
@@ -5,6 +5,7 @@ const path = std.fs.path;
 const File = std.Io.File;
 const Reader = std.Io.Reader;
 const Writer = std.Io.Writer;
+const Ast = std.zig.Ast;
 const eql = std.mem.eql;
 
 pub fn main(init: std.process.Init) !void {
@@ -49,6 +50,67 @@ fn process(init: std.process.Init, root_bindings: File, output_dir: Dir) !void {
     _ = output_dir;
 }
 
-fn parseNode(init : std.process.Init, ast: *std.zig.Ast, node_idx: std.zig.Ast.Node) !void {
+fn parseNode(init: std.process.Init, ast: *std.zig.Ast, node_idx: std.zig.Ast.Node) !void {
+    if (try parseVarDecl(init, ast, node_idx)) return;
+}
+
+// Keys have to be slices, values can just be node indexes,
+// since different node indexes resolving to the same thing won't resolve in a map.
+
+const FlagsDefinition = struct {
+    backing_type: Definition,
+    flags: std.StringArrayHashMapUnmanaged(Definition),
+};
+const EnumDefinition = struct {
+    backing_type: Definition,
+    values: std.StringArrayHashMapUnmanaged(Definition),
+};
+
+const Definition = union(enum) {
+    direct: Ast.Node.Index,
+    alias: []const u8,
+    flags: FlagsDefinition,
+    @"enum": EnumDefinition,
+};
+
+const Declaration = struct {
+    name: []const u8,
+    definition: Definition,
+};
+
+const Mapping = struct {
+    arena: std.mem.Allocator,
+    map: *std.StringArrayHashMapUnmanaged(Declaration),
+};
+
+const ParseUnit = struct {
+    ast: *Ast,
+    mapping: *Mapping,
+    node_idx: Ast.Node.Index,
+};
+
+fn caseVarDecl(ctx: ParseUnit) !void {
+    const could_be_var_decl = ctx.ast.fullVarDecl(ctx.node_idx);
+    const decl = if (could_be_var_decl) |vd| vd else return false;
     
+    const name_token = ctx.ast.tokenSlice(decl.ast.mut_token + 1);
+    const is_enum = std.mem.startsWith(u8, name_token, "enum_");
+
+    const is_flags, const is_alias, const is_direct = if (decl.ast.init_node.unwrap()) |ni| blk: {
+        if (ctx.ast.nodeTag(ni) != .identifier) break :blk .{ false, false, true };
+        const node_token = ctx.ast.tokenSlice(ctx.ast.firstToken(ni));
+        if (std.mem.eql(node_token, "WGPUFlags")) break :blk .{ true, false, false };
+        if (ctx.mapping.map.contains(node_token)) break :blk .{ false, true, false };
+        break :blk .{ false, false, true };
+    } else .{ false, false, false };
+    
+    if (is_enum) try parseEnumDecl(ctx)
+    else if (is_flags) try parseFlagsDecl(ctx)
+    else if (is_alias) try parseAliasDecl(ctx)
+    else if (is_direct) try parseDirectDecl(ctx)
+    else try handleUnknownDecl(ctx);
+}
+
+fn parseEnumDecl(ctx: ParseUnit) !void {
+    ctx.
 }
